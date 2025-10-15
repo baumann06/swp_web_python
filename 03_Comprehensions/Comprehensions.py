9aa295021d519ef724cdcd9b57d9df57cd956429
BaumCh <cbaumann253@gmail.com>
2025-10-15 14:56:07 +0200

    Update Comprehensions.py

--- 03_Comprehensions/Comprehensions.py
+++ 03_Comprehensions/Comprehensions.py
@@ -1,14 +1,23 @@
+def listComprehension(zahlen):
+    list_comp = [x * x for x in zahlen]
+    print(list_comp)
+
+def dictComprehension(zahlen):
+    dict_comp = {x: x * x for x in zahlen if x % 2 == 0}
+    print(dict_comp)
+
+def setComprehension(zahlen):
+    set_comp = {"gerade" if x % 2 == 0 else "ungerade" for x in zahlen}
+    print(set_comp)
+
 def main():
     zahlen = [1,2,3,4,5,6,7,8,9,10]
     #List Comprehension
-    list_quadrate = [x * x for x in zahlen]
-    print(list_quadrate)
+    listComprehension(zahlen)
     #Dict Comprehension
-    dict_quadrate = {x: x*x for x in zahlen if x%2 == 0}
-    print(dict_quadrate)
+    dictComprehension(zahlen)
     #Set Comprehension
-    set_quadrate = {"gerade" if x%2==0 else "ungerade" for x in zahlen}
-    print(set_quadrate)
+    setComprehension(zahlen)
 
 if __name__ == "__main__":
     main()
