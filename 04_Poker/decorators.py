2ae6b0f351f51b772fb32934aa4a44310ce995b0
BaumCh <cbaumann253@gmail.com>
2026-02-25 13:28:00 +0100

    Update decorators.py

--- 04_Poker/decorators.py
+++ 04_Poker/decorators.py
@@ -4,10 +4,10 @@ import time
 def timer(func):
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter()
+        start = time.perf_counter()
         result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        run_time = end_time - start_time
-        print(f"Finished {func.__name__}() in {run_time:.4f} secs")
+        end = time.perf_counter()
+        laufzeit = end - start
+        print(f"Finished {func.__name__}() in {laufzeit:.4f} secs")
         return result
     return wrapper_timer
